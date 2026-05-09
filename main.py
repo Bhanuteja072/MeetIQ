b129d90cb85ac275b9d1c675c7296df72fab29d6
Bhanteja072 <bhanutejapusarla@gmail.com>
2026-05-09 22:51:16 +0530

    Added domain name

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 # Allow React frontend to call this API later
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:8000","http://localhost:5173","https://meetiq-frontend-0j88.onrender.com"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:8000","http://localhost:5173","https://meetiq-frontend-0j88.onrender.com","https://meetiq.me","https://www.meetiq.me"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
