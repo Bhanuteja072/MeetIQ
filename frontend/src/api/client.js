a86f2ac89c50d8c89918ee6fc56ea1235c651e46
Bhanteja072 <bhanutejapusarla@gmail.com>
2026-05-10 13:18:46 +0530

    fix : hardcoded local url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,14 +1,11 @@
 import axios from 'axios'
-
+//Main
 // const api = axios.create({
 //   baseURL: `${import.meta.env.VITE_API_URL || ''}/api`,
 //   timeout: 120000,
 // })
-
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/api`
-    : 'https://meetiq-1-n0oi.onrender.com/api',
+  baseURL: 'https://meetiq-1-n0oi.onrender.com/api',
   timeout: 120000,
 })
 
